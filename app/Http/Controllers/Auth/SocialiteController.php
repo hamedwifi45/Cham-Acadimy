57eae52c0bc0641e8fbb3acffd14afdaedbcd9f3
hamedwifi45 <95859621+hamedwifi45@users.noreply.github.com>
2026-02-07 22:34:06 +0300

    ش

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -17,13 +17,11 @@ class SocialiteController extends Controller
         return Socialite::driver('google')->redirect();
     }
 
-    // معالجة العودة من Google
     public function handleGoogleCallback()
     {
         try {
             $googleUser = Socialite::driver('google')->user();
 
-            // البحث عن المستخدم في قاعدة البيانات
             $user = User::where('google_id', $googleUser->id)->first();
 
             $avatarUrl = $googleUser->getAvatar();
@@ -31,9 +29,6 @@ class SocialiteController extends Controller
             $filename = 'avatar_' . $googleUser->getId() . '.jpg';
 
             Storage::put('public/profile-photos/' . $filename, $avatarContents);
-
-            // dd($googleUser);
-            // إذا لم يوجد، ننشئه
             if (!$user) {
                 $user = User::create([
                     'name' => $googleUser->name,
@@ -45,7 +40,42 @@ class SocialiteController extends Controller
             }
             
 
-            // تسجيل الدخول
+            Auth::login($user);
+
+            return redirect('/');
+
+        } catch (\Exception $e) {
+            return redirect('/login')->withErrors('فشل تسجيل الدخول عبر جوجل');
+        }
+    }
+     public function redirectToFacebook()
+    {
+        return Socialite::driver('facebook')->redirect();
+    }
+
+    public function handleFacebookCallback()
+    {
+        try {
+            $facebookUser = Socialite::driver('facebook')->user();
+
+            $user = User::where('facebook_id', $facebookUser->id)->first();
+
+            $avatarUrl = $facebookUser->getAvatar();
+            $avatarContents = file_get_contents($avatarUrl);
+            $filename = 'avatar_' . $facebookUser->getId() . '.jpg';
+
+            Storage::put('public/profile-photos/' . $filename, $avatarContents);
+            if (!$user) {
+                $user = User::create([
+                    'name' => $facebookUser->name,
+                    'email' => $facebookUser->email,
+                    'facebook_id' => $facebookUser->id,
+                    'password' => bcrypt(Str::random(24)), // كلمة مرور عشوائية
+                    'profile_photo_path' => 'profile-photos/' . $filename,
+                ]);
+            }
+            
+
             Auth::login($user);
 
             return redirect('/');
